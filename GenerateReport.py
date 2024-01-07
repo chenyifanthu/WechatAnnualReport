96c72cd7d39f1bafd2ac1b6f7de06556d3c91895
ChenYifan <863506811@qq.com>
2024-01-07 17:00:26 +0800

    Fix some bugs.

--- GenerateReport.py
+++ GenerateReport.py
@@ -1,55 +1,16 @@
-import pandas as pd
-import pdb
+import re
 import time
-from tqdm import tqdm
-import matplotlib.pyplot as plt
 import jieba 
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from tqdm import tqdm
 from collections import Counter
 from wordcloud import WordCloud, STOPWORDS
+from preprocess import load_info, MY_WECHAT_NAME
 
 
-MY_WECHAT_NAME = "你的微信昵称"    # 用来替代聊天记录文件中的【我】 
-# 一些无聊的分词，不出现在词云中
-MY_STOPWORDS = ["一个", "这个", "真的", "可以", "就是", "没有", "不是", "我们", "还是", "现在", "应该", "什么", "可能", "这么", "他们", "那个", "知道", "感觉",
-                "已经", "但是", "不会", "觉得", "好像", "怎么", "还有", "哈哈哈哈", "明天", "你们", "一下", "消息", "不能", "今天", "而且", "一条", "开始", "直接", 
-                "撤回", "如果", "之前", "是不是", "所以", "因为", "的话", "然后", "时候", "这种", "为啥", "看到", "有点", "时间", "最后", "甚至", 
-                "\r\n", "之脑", "数字", "需要", "之后", "一些", "晚上", "下午", "必限", "大家", "大概", "必限任", "比较", "30", 
-                "今晚", "这边", "或者", "出来", "11", "那边", "", "", "", "", "", "", "", "", "", "", "", "", "", ]
-
-
-def remove_useless_msg(x: str):
-    if pd.isna(x):
-        return False
-    for black in ['<msg', '<?xml', 'http', '<a']:
-        if x.startswith(black):
-            return False
-    return True
-
-
-def load_info(contacts_file: str = 'contacts.csv', 
-                  messages_file: str = 'messages.csv'):
-    global contacts, messages
-    start_date = "2023-01-01"
-    end_date = "2023-12-30"
-    
-    contacts = pd.read_csv(contacts_file, index_col=False)
-    isgroup = {}
-    for i, row in contacts.iterrows():
-        isgroup[row['NickName']] = 'chatroom' in row['UserName']
-    messages = pd.read_csv(messages_file, index_col=False)
-    messages["isGroup"] = messages["NickName"].apply(lambda x: isgroup[x])
-    messages = messages[messages["StrContent"].apply(remove_useless_msg)]
-    messages = messages[(messages["StrTime"] >= start_date) & (messages["StrTime"] < end_date)]
-    messages["NickName"] = messages["NickName"].apply(str)
-    messages = messages.reset_index()
-    return contacts, messages
-
-def remark2name(remark: str):
-    global contacts
-    if remark == "我":
-        return MY_WECHAT_NAME
-    res = contacts[contacts["Remark"] == remark]["NickName"].values
-    return res[0] if len(res) > 0 else remark
 
 def filter_group(df: pd.DataFrame, group_name: str):
     res = df[df["NickName"].apply(lambda x: group_name in x)]
@@ -63,6 +24,7 @@ def calculate_words(df: pd.DataFrame):
     n_mess = len(df)
     n_char = sum(df['StrContent'].apply(len))
     return n_mess, n_char
+    
     
 def get_latest_time(df: pd.DataFrame, latest_hour: float = 5):
     def score(x):
@@ -86,6 +48,7 @@ def plot_nmess_per_minute(df: pd.DataFrame):
     plt.xticks(range(0, 25, 1))
     plt.savefig("nmess_per_minute.png")
     
+    
 def plot_nmess_per_month(df: pd.DataFrame):
     months = df.CreateTime.apply(lambda x: int(time.strftime("%m", time.localtime(x))))
     plt.figure(figsize=(12, 4))
@@ -96,13 +59,16 @@ def plot_nmess_per_month(df: pd.DataFrame):
 
 def plot_wordcloud(df):
     global STOPWORDS
+    my_stopwords = open("stopwords.txt", "r", encoding="utf-8").read().split("\n")
+    STOPWORDS |= set(my_stopwords + ["\r\n"])
+    
     all_words = []
-    STOPWORDS |= set(MY_STOPWORDS)
     for i, row in tqdm(df.iterrows(), total=len(df)):
         words = jieba.lcut(row["StrContent"])
         for word in words:
             if len(word) > 1 and word not in STOPWORDS:
                 all_words.append(word)
+                
     cnt = sorted(Counter(all_words).items(), key=lambda x: x[1], reverse=True)
     text = " ".join(all_words)
     wc = WordCloud(font_path="simhei.ttf", background_color="white", max_words=2000,
@@ -116,13 +82,14 @@ def plot_wordcloud(df):
     plt.savefig("wordcloud.png")
     return cnt
     
+    
 def most_active_person_in_group(df: pd.DataFrame, topk: int = 3):
     vc = df["Sender"].value_counts()
     name, n_mess = vc.index, vc.values
     results = []
     for i in range(topk):
         n_char = sum(df[df["Sender"] == name[i]]["StrContent"].apply(len))
-        results.append((remark2name(name[i]), n_mess[i], n_char))
+        results.append((name[i], n_mess[i], n_char))
     return results
 
 
@@ -131,8 +98,8 @@ def most_active_day(df):
     days = ymd.value_counts()
     return days.value_counts().sort_index().idxmax()
 
+
 def top_emoji(df: pd.DataFrame):
-    import re
     cnt = {}
     for i, row in df.iterrows():
         res_all = re.findall("\[.*?\]", row["StrContent"], re.I|re.M)
@@ -145,7 +112,7 @@ def top_emoji(df: pd.DataFrame):
 
 def personal_annual_report():
     global messages
-    me = messages[messages['Sender'] == '我']
+    me = messages[messages['Sender'] == MY_WECHAT_NAME]
     me = me.reset_index()
     n_mess, n_char = calculate_words(me)
     latest = get_latest_time(me)
@@ -187,6 +154,7 @@ def personal_annual_report():
     
     print("\n\n")
 
+
 def group_chat_annual_report(groupname):
     global messages
     group, fullname = filter_group(messages, groupname)
@@ -197,7 +165,7 @@ def group_chat_annual_report(groupname):
     print(f"👏群聊【{fullname}】2023年度报告\n")
     print("📊这一年中，我们在群里一共发出了{}条消息，{}个字".format(n_mess, n_char))
     print("  其中最晚的一条消息是【{}】在【{}】发出的，内容是【{}】".format(
-        remark2name(latest["Sender"]), latest["StrTime"], latest["StrContent"]))
+        latest["Sender"], latest["StrTime"], latest["StrContent"]))
     
     actives = most_active_person_in_group(group)
     print("\n🙋‍♂️本群水群小能手排行：")
@@ -218,13 +186,15 @@ def group_chat_annual_report(groupname):
     plot_nmess_per_minute(group)    
     plot_nmess_per_month(group)
     
+    
 def private_chat_annual_report(name):
     # TODO: 生成和某个人的聊天信息报告
     # 去跨年旅游懒得写了，欢迎大佬提交PR👏
     pass
     
     
+    
 if __name__ == "__main__":
     contacts, messages = load_info()
     personal_annual_report()
-    group_chat_annual_report("群聊名称")
+    # group_chat_annual_report("群聊名称")
---
+++ preprocess.py
@@ -0,0 +1,56 @@
+import re
+import pandas as pd
+
+MY_WECHAT_NAME = "my_wechat_name"    # 用来替代聊天记录文件中的【我】
+
+def parse_message(msg: str):
+    emoji_pattern = re.compile("<emoji .*?>")
+    voice_pattern = re.compile("<voicemsg .*?/>")
+    image_pattern = re.compile("<img .*?/>")
+    video_pattern = re.compile("<videomsg .*?/>")
+    location_pattern = re.compile("<location .*?")
+    card_pattern = re.compile("<msg.*?username=.*?>")
+    sys_pattern = re.compile("<a href=.*?</a>")
+    
+    for pattern in [emoji_pattern, voice_pattern, image_pattern, video_pattern, 
+                    location_pattern, card_pattern, sys_pattern]:
+        if pattern.search(msg) is not None:
+            return False
+    return True
+
+
+def load_info(contacts_file: str = 'contacts.csv', 
+              messages_file: str = 'messages.csv',
+              start_date: str = "2023-01-01",
+              end_date: str = "2024-01-01"):
+    global contacts, messages
+    contacts = pd.read_csv(contacts_file, index_col=False)
+    
+    # 区分群聊和联系人
+    isgroup = {}
+    for i, row in contacts.iterrows():
+        isgroup[row['NickName']] = 'chatroom' in row['UserName']
+        
+    # 将备注名转换为微信昵称
+    remark2nickname = {'我': MY_WECHAT_NAME}
+    for i, row in contacts.iterrows():
+        if not isgroup[row['NickName']] and row['Remark']:
+            remark2nickname[row['Remark']] = row['NickName']
+        
+    messages = pd.read_csv(messages_file, index_col=False)
+    messages.dropna(subset=["StrContent", "NickName", "StrTime"], inplace=True)
+    messages = messages[(messages["StrTime"] >= start_date) & \
+                        (messages["StrTime"] < end_date) & \
+                        (messages["StrContent"].apply(parse_message))]
+    
+    messages["isGroup"] = messages["NickName"].apply(lambda x: isgroup[x])
+    messages["Sender"] = messages["Sender"].apply(lambda x: remark2nickname.get(x, x))
+    messages.reset_index(inplace=True)
+    
+    return contacts, messages
+
+
+
+if __name__ == "__main__":
+    contacts, messages = load_info()
+    print(messages.head())
